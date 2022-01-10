7d9764786edc82c135075090d10954a72e3ef5a4
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-01-10 17:25:42 -0500

    Add svg icon to delete button

--- src/task.js
+++ src/task.js
@@ -1,10 +1,15 @@
+import { FaTrashAlt } from 'react-icons/fa';
+
 function TaskItem(props) {
   return (
     props.list.map((activity) => (
       <li key={activity.id}>
         <input type="checkbox" checked={activity.done} onChange={() => props.checkmark(activity.id)} />
         <input type="text" placeholder={activity.task} />
-        <button type="button" className="deleteButton" onClick={() => props.delete(activity.id)}>&#128465;</button>
+        <button type="button" className="deleteButton" onClick={() => props.delete(activity.id)}>
+          <FaTrashAlt />
+          {' '}
+        </button>
       </li>
     ))
   );
