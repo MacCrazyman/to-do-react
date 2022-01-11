0354138c7233228782fc6dbe7634ab1ea001b4f6
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-01-11 10:45:45 -0500

    Add error page

---
+++ src/pages/notFound.jsx
@@ -0,0 +1,10 @@
+import './notfound.css'
+
+const ErrorPage = () => {
+  return (
+    <div className="container">
+      <h2 className="message">404</h2>
+      <p className='paragraph'>Page not found</p>
+    </div>
+  )
+}
