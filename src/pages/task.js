144090a50ce75157580b00d16f46d162cd2a3abb
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-01-13 18:37:05 -0500

    Add feature to mark activities as completed

--- src/pages/task.js
+++ src/pages/task.js
@@ -1,11 +1,12 @@
 import { FaTrashAlt } from 'react-icons/fa';
+import './task.css';
 
 function TaskItem(props) {
   return (
     props.list.map((activity) => (
       <li key={activity.id}>
         <input type="checkbox" checked={activity.done} onChange={() => props.checkmark(activity.id)} />
-        <input type="text" placeholder={activity.task} />
+        <input type="text" placeholder={activity.task} className={activity.done ? 'marked' : 'normal'} />
         <button type="button" className="deleteButton" onClick={() => props.delete(activity.id)}>
           <FaTrashAlt />
           {' '}
