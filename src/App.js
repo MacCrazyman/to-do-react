ab203ecaf76e9bb2ca18ed74570a8a9a07131172
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-01-13 19:39:29 -0500

    Added basic styling to NavBar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <nav>
-        <ul>
+        <ul className="navigation">
           <li>
             <Link to="about"> About</Link>
           </li>
